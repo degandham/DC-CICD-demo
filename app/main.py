83fece65d1d0bcec01ff3d06023bf439f153f046
pdutta2016 <pinak.dutta@gmail.com>
2017-06-15 14:36:47 -0400

    changes

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def sample():
-    return "<h2 style='color:blue'>Welcome to Deloitte Cloud CICD Demo - This is Change#1 <a href="https://www.w3schools.com">Visit W3Schools</a></h2>"
+    return "<h2 style='color:blue'>Welcome to Deloitte Cloud CICD Demo - This is Change#1 </h2>"
 
 if __name__ == '__main__':
     app.run()
