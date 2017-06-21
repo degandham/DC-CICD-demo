9a185de22f96d258b4cae3348e4a8fbdfd519b9c
pdutta2016 <pinak.dutta@gmail.com>
2017-06-21 11:05:42 -0400

    more changes

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def sample():
-    return "<h2 style='color:blue'>Welcome to Deloitte Cloud CICD Demo - This is Change#1 </h2>
+    return "<h2 style='color:blue'>Welcome to Deloitte Cloud CICD Demo - This is Change#1 </h2>"
 
 def return_hostname():
-    return "<h2 style='color:green'>socket.gethostname()</h2>
+    return "<h2 style='color:green'>socket.gethostname()</h2>"
     
 if __name__ == '__main__':
     app.run()
