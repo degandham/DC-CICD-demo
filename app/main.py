e16e14746eee9179841f2c6b0e90fd3130f50eae
pdutta2016 <pinak.dutta@gmail.com>
2017-06-21 09:10:54 -0400

    revert

--- app/main.py
+++ app/main.py
@@ -1,13 +1,9 @@
-import socket
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def sample():
-    return "<h2 style='color:blue'>Welcome to Deloitte Cloud CICD Demo - This is Change#1 </h2>"
-
-def return_hostname():
-    return "This is an example wsgi app served from {} to {}".format(socket.gethostname(), request.remote_addr)
+    return "<h2 style='color:blue'>Welcome to Deloitte Cloud CICD Demo - This is Change#1 </h2>
 
 if __name__ == '__main__':
     app.run()
