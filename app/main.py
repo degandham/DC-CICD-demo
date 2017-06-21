989a99149f889cd32421cc528c958607aa02c924
pdutta2016 <pinak.dutta@gmail.com>
2017-06-21 11:56:33 -0400

    changes

--- app/main.py
+++ app/main.py
@@ -1,13 +1,9 @@
-import socket
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def sample():
     return "<h2 style='color:blue'>Welcome to Deloitte Cloud CICD Demo - This is Change#1 </h2>"
-
-def return_hostname():
-    return "<h2 style='color:green'>socket.gethostname()</h2>"
     
 if __name__ == '__main__':
     app.run()
