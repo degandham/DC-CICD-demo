7259d9072af99be0e736710ba5162a96a2444ee4
pdutta2016 <pinak.dutta@gmail.com>
2017-06-26 21:35:52 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def sample():
     return "<body style='background-color:red;'><p style='text-align:center'><font face='verdana' color='black' size='6'>Deloitte Cloud Demo Client Web App</p></font></body>"
-    
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
 if __name__ == '__main__':
     app.run()
