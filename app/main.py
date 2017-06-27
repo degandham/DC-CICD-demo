ca277b004febadcac3662a8cd9b005156629976f
pdutta2016 <pinak.dutta@gmail.com>
2017-06-26 21:08:50 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def sample():
-    return "<body style='background-color:red;'><p style='text-align:center'><font face='verdana' color='black' size='6'>Deloitte Cloud Demo Client Web App</p></font></body>"
+    return "<body style='background-color:red;'><p style='text-align:center'><font face='verdana' color='black' size='6'>Deloitte Cloud Demo Client Web App</p></font><p><script src=‘/home/ec2-user/env.js’></script><script>console.log(env.DEMO_ENV_TYPE)</script></p></body>"
     
 if __name__ == '__main__':
     app.run()
