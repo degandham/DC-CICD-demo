7748a5e4ec5ce5c337103f789aafa22cd05c8acf
pdutta2016 <pinak.dutta@gmail.com>
2017-06-26 20:09:00 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def sample():
-    return "<body style='background-color:powderblue;'><h1 style='font-family:calibri color:black font-size:300%'>Deloitte Cloud DevOps Demo Website</h1></body>"
+    return "<body style='background-color:powderblue;'><h1 style='font-family:calibri>Deloitte Cloud DevOps Demo Website</h1><p>About us</p></body>"
     
 if __name__ == '__main__':
     app.run()
