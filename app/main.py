9d932c666e713749e6684bcd63d3e130eb3cef3b
pdutta2016 <pinak.dutta@gmail.com>
2017-06-26 21:30:00 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def sample():
-    return "<body style='background-color:red;'><p style='text-align:center'><font face='verdana' color='black' size='6'>Deloitte Cloud Demo Client Web App</p></font><p><script>console.log(env.DEMO_ENV_TYPE)</script></p></body>"
+    return "<body style='background-color:red;'><p style='text-align:center'><font face='verdana' color='black' size='6'>Deloitte Cloud Demo Client Web App</p></font></body>"
+    print os.environ['DEMO_ENV_TYPE']
     
 if __name__ == '__main__':
     app.run()
