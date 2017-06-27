212df9734c2bc4b635bfb20af1aa4979bd5bbf15
pdutta2016 <pinak.dutta@gmail.com>
2017-06-26 21:33:20 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def sample():
-    return "<body style='background-color:red;'><p style='text-align:center'><font face='verdana' color='black' size='6'>Deloitte Cloud Demo Client Web App</p></font></body>"
-    print os.environ["DEMO_ENV_TYPE"]
+    return "<body style='background-color:red;'><p style='text-align:center'><font face='verdana' color='black' size='6'>Deloitte Cloud Demo Client Web App os.environ["DEMO_ENV_TYPE"]</p></font></body>"
     
 if __name__ == '__main__':
     app.run()
